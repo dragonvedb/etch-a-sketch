ebfe9267c9f968513d7c2bbe3f1cded9f0e17013
Daniel Bronsky <dragonvedb@gmail.com>
2022-12-07 22:39:10 +0300

    Make a function that takes a resolution value and creates an appropriate amount of pixels

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const container = document.querySelector('#container')
 
-container.style.backgroundColor = 'blue'
+function generateCanvas(res) {
+    const pixelSize = 800 / res;
+    const pixelNumber = res * res;
+
+    for (let i = 0; i < pixelNumber; i++) {
+        const pixel = document.createElement('div');
+        pixel.style.cssText = `width: ${pixelSize}px; heigth: ${pixelSize}px;`;
+        //pixel.textContent = `${i+1}`
+        container.appendChild(pixel);
+    }
+}
+
+generateCanvas(16);
